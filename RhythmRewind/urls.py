7e9326f24e579de578f046fc6c41efd05700bc03
kristenthon <kristenthon22@gmail.com>
2024-11-14 14:12:49 -0500

    UI for homepage

--- RhythmRewind/urls.py
+++ RhythmRewind/urls.py
@@ -17,7 +17,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+
+
 urlpatterns = [
+
+    path('', include('pages.urls')),
     path('accounts/', include('allauth.urls')),
     path('admin/', admin.site.urls),
+
 ]
